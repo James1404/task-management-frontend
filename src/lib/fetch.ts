70771b0a8df0e3df793c101d3a26767b9f7f63be
James Barnfather <jamesbarnfather99@gmail.com>
2026-03-11 18:07:29 +0000

    Moved libs to lib folder

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,42 +1,39 @@
-import createClient, {
-    createPathBasedClient,
-    type Middleware,
-} from "openapi-fetch";
+import createClient, { type Middleware } from "openapi-fetch";
 import type { paths } from "../../generated/backend";
-import useCredentials, {
-    getAuthorizationHeader,
-    setAccess,
-} from "../stores/credentials";
+import { getAuthorizationHeader, setAccess } from "../stores/credentials";
 
 async function refresh() {
-    const access = useCredentials.getState().access;
-
-    const { data, error } = await client.POST("/auth/refresh", {
+    const { data, error, response } = await client.POST("/v1/auth/refresh", {
         headers: { Authorization: getAuthorizationHeader() },
         credentials: "include",
     });
 
-    if (error) {
-        return;
-    }
-
-    if (data == undefined) {
-        return;
+    if (error || data == undefined) {
+        return response;
     }
 
     setAccess(data.access);
 }
 
+const NonAuthPaths = [
+    "/v1/auth/refresh",
+    "/v1/auth/login",
+    "/v1/auth/register",
+];
+
 const authMiddleware: Middleware = {
-    async onRequest({ request, options }) {
-        const access = useCredentials.getState().access;
+    async onRequest({ request, schemaPath }) {
+        if (NonAuthPaths.includes(schemaPath)) {
+            return;
+        }
+
         request.headers.set("Authorization", getAuthorizationHeader());
     },
 
-    async onResponse({ request, response }) {
+    async onResponse({ request, response, schemaPath }) {
         const { body, status, ...resOptions } = response;
 
-        if (status == 401) {
+        if (status === 401 && !NonAuthPaths.includes(schemaPath)) {
             await refresh();
         }
 
