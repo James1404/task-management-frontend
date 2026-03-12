366fe1dbf7479d0c0824adb482faf73976e77dce
James Barnfather <jamesbarnfather99@gmail.com>
2026-03-12 19:05:47 +0000

    Created a folder for reusable react queries, helps with caching

---
+++ src/queries/projects.ts
@@ -0,0 +1,33 @@
+import { createProject, getProject, getProjects } from "@/api/projects";
+import type { ProjectDataSchemaType } from "@/schemas/project.schema";
+import {
+    mutationOptions,
+    queryOptions,
+    useQueryClient,
+} from "@tanstack/react-query";
+
+export function currentProjectOptions(projectId: number) {
+    return queryOptions({
+        queryKey: ["currentProject"],
+        queryFn: async () => getProject(projectId),
+        staleTime: 600000,
+    });
+}
+
+export function getAllProjectsOptions() {
+    return queryOptions({
+        queryKey: ["projects"],
+        queryFn: async () => await getProjects(),
+    });
+}
+
+export function createProjectOptions() {
+    const queryClient = useQueryClient();
+
+    return mutationOptions({
+        mutationFn: async (body: ProjectDataSchemaType) =>
+            await createProject(body),
+        onSuccess: async () =>
+            await queryClient.invalidateQueries({ queryKey: ["projects"] }),
+    });
+}
