9a8bd50b36637b3a3854614e7c237219423c2285
James Barnfather <jamesbarnfather99@gmail.com>
2026-04-08 18:14:43 +0100

    Removed unused onLogout function

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,17 +1,10 @@
-import { createRootRoute, Outlet, useNavigate } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { ThemeProvider } from "@/components/theme-provider";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/sonner";
-import { logout } from "@/api/user.api";
 
 function RootLayout() {
-    const navigate = useNavigate({});
-    const onLogout = async () => {
-        await logout();
-        navigate({ to: "/" });
-    };
-
     return (
         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
             <TooltipProvider>
