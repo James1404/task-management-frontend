65d324b0e2437f407623b0be4d420aa946707a61
James Barnfather <jamesbarnfather99@gmail.com>
2026-04-15 15:28:14 +0100

    Fixed schema issue

--- src/schemas/columns.schema.ts
+++ src/schemas/columns.schema.ts
@@ -7,7 +7,6 @@ export type OrderType = z.infer<typeof OrderSchema>;
 
 export const ColumnDataSchema = z.object({
     name: z.string().min(3),
-    order: OrderSchema,
 });
 
 export type ColumnDataSchemaType = z.infer<typeof ColumnDataSchema>;
@@ -16,6 +15,7 @@ export const ColumnSchema = z.intersection(
     ColumnDataSchema,
     z.object({
         id: z.string(),
+        order: OrderSchema,
     }),
 );
 export type ColumnSchemaType = z.infer<typeof ColumnSchema>;
