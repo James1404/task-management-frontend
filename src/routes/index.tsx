2b4324233cec938cfb3f8638603cebac9fe0ef2f
James Barnfather <jamesbarnfather99@gmail.com>
2026-04-23 20:43:48 +0100

    Updated home page.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
-import LoginField from "@/components/login-field";
 import { loggedIn } from "@/stores/credentials";
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { createFileRoute, Link, redirect } from "@tanstack/react-router";
+import { Button } from "@/components/ui/button";
 
 export const Route = createFileRoute("/")({
     component: Index,
@@ -18,10 +18,20 @@ export const Route = createFileRoute("/")({
 
 function Index() {
     return (
-        <div className="p-2">
-            <h3>Welcome Home!</h3>
+        <main className="max-w-200 mx-auto p-5 min-h-screen flex flex-col items-center gap-5">
+            <header className="flex flex-col justify-center items-center gap-5 h-screen">
+                <h1 className="text-5xl">Task Management</h1>
+                <p className="text-xl">By James Barnfather</p>
 
-            <LoginField />
-        </div>
+                <div className="flex flex-row gap-2">
+                    <Button asChild variant="default">
+                        <Link to="/login">Login</Link>
+                    </Button>
+                    <Button asChild variant="secondary">
+                        <Link to="/register">Register</Link>
+                    </Button>
+                </div>
+            </header>
+        </main>
     );
 }
